86d31490152ac743d40ab3a8ebef89403408ef44
svc-student <svc-student@student.directory.jccc>
2019-03-28 19:10:48 -0500

    inclass work 3-28

---
+++ InClass3-28/Assets/Editor/ColorableEditor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Colorable))]
+[CanEditMultipleObjects]
+public class ColorableEditor : Editor
+{
+  private readonly string lable = "My Color";
+  int drawCount = 0;
+
+  public override void OnInspectorGUI()
+  {
+    FoldoutAttempt();
+  }
+
+  private void BaseChangeColor()
+  {
+    drawCount++;
+    Colorable myTarget = target as Colorable;
+
+    Color currentColor = myTarget.GetComponent<MeshRenderer>().material.color;
+
+    currentColor = EditorGUILayout.ColorField(lable, currentColor);
+    currentColor.a = EditorGUILayout.Slider("Transparency", currentColor.a, 0.0F, 1.0F);
+
+    myTarget.GetComponent<MeshRenderer>().material.color = currentColor;
+
+    EditorGUILayout.LabelField(drawCount.ToString());
+  }
+
+  bool ShowColorEditing;
+
+  private void FoldoutAttempt()
+  {
+    ShowColorEditing = EditorGUILayout.Foldout(ShowColorEditing, lable);
+    if (ShowColorEditing)
+    {
+      if (Selection.activeTransform)
+      {
+        drawCount++;
+        Colorable myTarget = target as Colorable;
+
+        Color currentColor = myTarget.GetComponent<MeshRenderer>().material.color;
+
+        currentColor = EditorGUILayout.ColorField(lable, currentColor);
+        currentColor.a = EditorGUILayout.Slider("Transparency", currentColor.a, 0.0F, 1.0F);
+
+        myTarget.GetComponent<MeshRenderer>().material.color = currentColor;
+      }
+    }
+
+    EditorGUILayout.LabelField(drawCount.ToString());
+  }
+
+}
